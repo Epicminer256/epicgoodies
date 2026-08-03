a30f87706c621156da2596ee976f55edff5012ef
EpicMiner256 <72170170+Epicminer256@users.noreply.github.com>
2026-08-03 03:13:05 -0400

    Add files via upload

--- init.py
+++ init.py
@@ -1,13 +1,41 @@
 # pip install internetarchive
 
 import os
+import time
 import json
 import internetarchive
 from pathlib import Path
 
 masterurls = [
 	["world-ends-with-you-the", "nds"],
-	["psx-chd-roms-b", "psx"]
+    ["blockbuster-competition-2-nba-jam-judge-dredd-jue", "md"]
+    ["MySnesRoms", "smc"]
+	["psx-chd-roms-a", "psx"],
+	["psx-chd-roms-b", "psx"],
+	["psx-chd-roms-c", "psx"],
+	["psx-chd-roms-d", "psx"],
+	["psx-chd-roms-e", "psx"],
+	["psx-chd-roms-f", "psx"],
+	["psx-chd-roms-g", "psx"],
+	["psx-chd-roms-h", "psx"],
+	["psx-chd-roms-i", "psx"],
+	["psx-chd-roms-j", "psx"],
+	["psx-chd-roms-k", "psx"],
+	["psx-chd-roms-l", "psx"],
+	["psx-chd-roms-m", "psx"],
+	["psx-chd-roms-n", "psx"],
+	["psx-chd-roms-o", "psx"],
+	["psx-chd-roms-p", "psx"],
+	["psx-chd-roms-q", "psx"],
+	["psx-chd-roms-r", "psx"],
+	["psx-chd-roms-s", "psx"],
+	["psx-chd-roms-t", "psx"],
+	["psx-chd-roms-u", "psx"],
+	["psx-chd-roms-v", "psx"],
+	["psx-chd-roms-w", "psx"],
+	["psx-chd-roms-x", "psx"],
+	["psx-chd-roms-y", "psx"],
+	["psx-chd-roms-z", "psx"]
 ]
 
 fileTypesAccepted = [
@@ -50,6 +78,7 @@ fileTypesAccepted = [
 templatejson = json.loads(open("base.json").read())
 
 for u in range(0, len(masterurls)):
+    print("working on "+masterurls[u][0])
     item=internetarchive.get_item(masterurls[u][0])
     downloadpath="sdmc:/roms/"+masterurls[u][1]
     files = item.get_files()
@@ -65,12 +94,15 @@ for u in range(0, len(masterurls)):
                         "title": clean_name,
                         "version": "v1"
                     },
-                    "script":[{
+                    "Download File":[{
                         "file": file.url,
                         "message": "Downloading...",
                         "output": downloadpath+"/"+clean_name,
                         "type": "downloadFile"
                     }]
                 })
+    print("sleeping for 5 seconds...")
+    time.sleep(5)
+    print("continuing")
 
 open("epicgoodies.unistore", "w").write(json.dumps(templatejson, indent="\t"))
