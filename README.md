cd2d8d87360ebb6d99f63575f1c1102963ab6139
EpicMiner256 <72170170+Epicminer256@users.noreply.github.com>
2026-08-03 23:38:48 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # epicgoodies
+<<<<<<< HEAD
 a universal updator repo for roms and such
 
+=======
+Open Universal Updator, and add the qr codes from my github webpage as a unistore.
+>>>>>>> 3adbcd8 (test)
--- init.py
+++ init.py
@@ -11,21 +11,33 @@ from bs4 import BeautifulSoup
 
 masterurls = [
         [
+<<<<<<< HEAD
             "psx_and_n64.unistore",
+=======
+            "PSX_And_N64.unistore",
+>>>>>>> 3adbcd8 (test)
             [
                 ["n64-roms_202405", "n64"],
                 ["psx_chd_torrent_2_20220105","psx"],
             ]
         ],
         [
+<<<<<<< HEAD
             "handhelds.unistore",
             [
                 ["gbc_roms_202605", "gbc"],
+=======
+            "Gameboy_Roms.unistore",
+            [
+                # This one is spanish ["gbc_roms_202605", "gbc"],
+                ["gbc-roms", "gbc"], # High file size
+>>>>>>> 3adbcd8 (test)
                 ["tsf-2002-gba-roms-collection", "gba"],
                 ["gameboy-best-roms", "gb"]
             ]
         ],
         [
+<<<<<<< HEAD
             "psx_and_n64.unistore",
             [
                 ["n64-roms_202405", "n64"],
@@ -35,6 +47,27 @@ masterurls = [
 ]
 
 fileTypesAccepted = [
+=======
+            "Nintendo_Console_Roms.unistore",
+            [
+                ["action-52_202405", "nes"],
+                ["MySnesRoms", "smc"]
+            ]
+        ],
+        [
+            "Sega_Console_Roms.unistore",
+            [
+                ["nointro.md", "md"],
+            ]
+        ]
+]
+
+fileTypesAccepted = [
+    # i might remove this sometime, but some games are only in zip cause thats what people do
+    "zip",
+    "7z",
+
+>>>>>>> 3adbcd8 (test)
 	"3ds",
 	"3dsx",
 	"cia",
@@ -74,7 +107,11 @@ fileTypesAccepted = [
 ]
 
 def url_to_qr(url):
+<<<<<<< HEAD
     return """<img width="400px" src="https://public-api.qr-code-generator.com/v1/create/extended?image_format=PNG&image_width=300&qr_code_text="""+quote(url)+"""&foreground_color=%23000000&background_color=%23FFFFFF&frame_name=no-frame">"""
+=======
+    return """<img width="300px" src="https://public-api.qr-code-generator.com/v1/create/extended?image_format=PNG&image_width=300&qr_code_text="""+quote(url)+"""&foreground_color=%23000000&background_color=%23FFFFFF&frame_name=no-frame">"""
+>>>>>>> 3adbcd8 (test)
 
 templatejson = json.loads(open("base.json").read())
 templatejson["storeInfo"]["revision"]+=1
@@ -92,7 +129,11 @@ for m in range(0, len(masterurls)):
     current_unistore = masterurls[m]
     unistore_file = current_unistore[0]
 
+<<<<<<< HEAD
     inject_html += "<h2>"+unistore_file+"</h2><br>"+url_to_qr(unistore_file)
+=======
+    inject_html += "<div style='display:inline'> <h2>"+unistore_file+"</h2>"+url_to_qr("https://github.com/Epicminer256/epicgoodies/raw/refs/heads/main/"+unistore_file)+"</div>"
+>>>>>>> 3adbcd8 (test)
 
     templatejson["storeInfo"]["title"] = unistore_file
     templatejson["storeInfo"]["file"] = unistore_file
@@ -107,6 +148,10 @@ for m in range(0, len(masterurls)):
         item=internetarchive.get_item(current_masterurl)
         downloadpath="sdmc:/roms/"+current_filetype
         files = item.get_files()
+<<<<<<< HEAD
+=======
+        print("This URL has "+str(len(list(files)))+" files total")
+>>>>>>> 3adbcd8 (test)
         for file in files:
             clean_name = os.path.basename(file.name)
             for ext in fileTypesAccepted:
@@ -132,8 +177,14 @@ for m in range(0, len(masterurls)):
     open(unistore_file, "w").write(json.dumps(templatejson))
 
 original_html = BeautifulSoup(open("index.html", "r").read(), 'lxml')
+<<<<<<< HEAD
 inject_html = BeautifulSoup(inject_html, "html.parser")
 original_html.find("main").append(inject_html)
+=======
+inject_html = "<main>"+inject_html+"</main>"
+inject_html = BeautifulSoup(inject_html, "html.parser")
+original_html.find("main").replace_with(inject_html)
+>>>>>>> 3adbcd8 (test)
 
 open("index.html", "w").write(original_html.prettify())
 
