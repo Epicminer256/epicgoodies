0c56049e40539537af4b94ff2c9b785b2d207714
Your Name <you@example.com>
2026-08-04 00:41:23 -0400

    fix

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # epicgoodies
-<<<<<<< HEAD
-a universal updator repo for roms and such
-
-=======
 Open Universal Updator, and add the qr codes from my github webpage as a unistore.
->>>>>>> 3adbcd8 (test)
--- init.py
+++ init.py
@@ -11,43 +11,22 @@ from bs4 import BeautifulSoup
 
 masterurls = [
         [
-<<<<<<< HEAD
-            "psx_and_n64.unistore",
-=======
             "PSX_And_N64.unistore",
->>>>>>> 3adbcd8 (test)
             [
                 ["n64-roms_202405", "n64"],
                 ["psx_chd_torrent_2_20220105","psx"],
             ]
         ],
         [
-<<<<<<< HEAD
-            "handhelds.unistore",
-            [
-                ["gbc_roms_202605", "gbc"],
-=======
             "Gameboy_Roms.unistore",
             [
                 # This one is spanish ["gbc_roms_202605", "gbc"],
                 ["gbc-roms", "gbc"], # High file size
->>>>>>> 3adbcd8 (test)
                 ["tsf-2002-gba-roms-collection", "gba"],
                 ["gameboy-best-roms", "gb"]
             ]
         ],
         [
-<<<<<<< HEAD
-            "psx_and_n64.unistore",
-            [
-                ["n64-roms_202405", "n64"],
-                ["psx_chd_torrent_2_20220105","psx"],
-            ]
-        ],
-]
-
-fileTypesAccepted = [
-=======
             "Nintendo_Console_Roms.unistore",
             [
                 ["action-52_202405", "nes"],
@@ -67,7 +46,6 @@ fileTypesAccepted = [
     "zip",
     "7z",
 
->>>>>>> 3adbcd8 (test)
 	"3ds",
 	"3dsx",
 	"cia",
@@ -107,11 +85,7 @@ fileTypesAccepted = [
 ]
 
 def url_to_qr(url):
-<<<<<<< HEAD
-    return """<img width="400px" src="https://public-api.qr-code-generator.com/v1/create/extended?image_format=PNG&image_width=300&qr_code_text="""+quote(url)+"""&foreground_color=%23000000&background_color=%23FFFFFF&frame_name=no-frame">"""
-=======
     return """<img width="300px" src="https://public-api.qr-code-generator.com/v1/create/extended?image_format=PNG&image_width=300&qr_code_text="""+quote(url)+"""&foreground_color=%23000000&background_color=%23FFFFFF&frame_name=no-frame">"""
->>>>>>> 3adbcd8 (test)
 
 templatejson = json.loads(open("base.json").read())
 templatejson["storeInfo"]["revision"]+=1
@@ -129,11 +103,7 @@ for m in range(0, len(masterurls)):
     current_unistore = masterurls[m]
     unistore_file = current_unistore[0]
 
-<<<<<<< HEAD
-    inject_html += "<h2>"+unistore_file+"</h2><br>"+url_to_qr(unistore_file)
-=======
     inject_html += "<div style='display:inline'> <h2>"+unistore_file+"</h2>"+url_to_qr("https://github.com/Epicminer256/epicgoodies/raw/refs/heads/main/"+unistore_file)+"</div>"
->>>>>>> 3adbcd8 (test)
 
     templatejson["storeInfo"]["title"] = unistore_file
     templatejson["storeInfo"]["file"] = unistore_file
@@ -148,10 +118,7 @@ for m in range(0, len(masterurls)):
         item=internetarchive.get_item(current_masterurl)
         downloadpath="sdmc:/roms/"+current_filetype
         files = item.get_files()
-<<<<<<< HEAD
-=======
         print("This URL has "+str(len(list(files)))+" files total")
->>>>>>> 3adbcd8 (test)
         for file in files:
             clean_name = os.path.basename(file.name)
             for ext in fileTypesAccepted:
@@ -177,14 +144,9 @@ for m in range(0, len(masterurls)):
     open(unistore_file, "w").write(json.dumps(templatejson))
 
 original_html = BeautifulSoup(open("index.html", "r").read(), 'lxml')
-<<<<<<< HEAD
-inject_html = BeautifulSoup(inject_html, "html.parser")
-original_html.find("main").append(inject_html)
-=======
 inject_html = "<main>"+inject_html+"</main>"
 inject_html = BeautifulSoup(inject_html, "html.parser")
 original_html.find("main").replace_with(inject_html)
->>>>>>> 3adbcd8 (test)
 
 open("index.html", "w").write(original_html.prettify())
 
